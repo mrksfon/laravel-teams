15fc00f182d63ea7102abde82954f622125e857a
mrksfon <markostevic96@hotmail.com>
2025-03-04 18:18:52 +0100

    Making team members look better

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,12 @@ class User extends Authenticatable
         ];
     }
 
+    public function profilePhotoUrl()
+    {
+        return 'https://gravatar.com/avatar/'.md5($this->email).'?s=100';
+    }
+
+
     public function teams()
     {
         return $this->belongsToMany(Team::class);
